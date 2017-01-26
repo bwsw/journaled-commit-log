9962c4ef9adc304c03047b8602149f1156186aba
Bitworks Software, Ltd <bitworks@bw-sw.com>
2017-01-26 21:42:33 +0700

    Implemented FilePathManager for easy management of directory paths and log paths.

---
+++ src/main/scala/com/bwsw/commitlog/FilePathManager.scala
@@ -0,0 +1,66 @@
+package com.bwsw.commitlog
+
+import java.io.{File, FilenameFilter}
+import java.nio.file.Paths
+import java.text.SimpleDateFormat
+import java.util.Calendar
+
+/**
+  * Created by Ivan Kudryavtsev on 26.01.2017.
+  */
+class FilePathManager(rootDir: String) {
+  private var curDate: String = new SimpleDateFormat("yyyy/MM/dd").format(Calendar.getInstance.getTime)
+  private val rootPath = new File(rootDir)
+  private var nextID = -1
+  private val ext = ".dat"
+  private val datFilter = new FilenameFilter() {
+    override def accept(dir: File, name: String): Boolean = {
+      println(name)
+      name.toLowerCase().endsWith(ext)
+    }
+  }
+
+  if(!rootPath.isDirectory())
+    throw new IllegalArgumentException(s"Path $rootDir doesn't exists.")
+
+  def getNextPath(): String = {
+
+    val testDate: String = new SimpleDateFormat("yyyy/MM/dd").format(Calendar.getInstance.getTime)
+
+    if(curDate != testDate) {
+      nextID = -1
+      curDate = testDate
+    } else {
+      if(nextID >= 0) {
+        val nextPath = Paths.get(testDate, nextID.toString).toString
+        nextID += 1
+        return nextPath
+      }
+    }
+
+    if(createPath()) {
+      nextID = 0
+      return Paths.get(rootDir, testDate, nextID.toString).toString
+    } else {
+      val filesDir = new File(Paths.get(rootDir, testDate).toString)
+      var max = -1
+
+      filesDir.listFiles(datFilter).foreach(f => {
+        if (Integer.parseInt(f.getName.split("\\.")(0)) > max) max = Integer.parseInt(f.getName.split("\\.")(0))
+        println(f)
+      })
+      nextID = max + 1
+      return Paths.get(rootDir, testDate, nextID.toString).toString
+    }
+  }
+
+  def createPath() = {
+    val path = new File(Paths.get(rootDir, curDate).toString)
+    if(!(path.exists() && path.isDirectory())) {
+      path.mkdirs()
+      true
+    } else
+      false
+  }
+
+}
